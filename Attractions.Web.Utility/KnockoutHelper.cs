c0f822ba4a0bb1f7c7a6fc165c831bdf87765ed6
aramkoukia <aramkoukia@gmail.com>
2014-09-09 10:16:27 -0700

    Knockout support

---
+++ Attractions.Web.Utility/KnockoutHelper.cs
@@ -0,0 +1,39 @@
+﻿using Newtonsoft.Json;
+using System;
+
+namespace Attractions.Web.Utility
+{
+    /// <summary>
+    /// Summary description for KnockoutHelper
+    /// </summary>
+    public class KnockoutHelper
+    {
+	    public KnockoutHelper(){
+	    }
+
+        public static string CreateKnockoutContext()
+        {
+            return @"<script>$(document).ready(function () {{                                                                        
+                                 if (portalViewModel.PageData != null){{
+                                    portalViewModel.PageData.Data = ko.mapping.fromJS(viewModelJs);
+                                    if (portalViewModel.PageData.DataLoaded != null){{
+                                        portalViewModel.PageData.DataLoaded();
+                                    }}
+                                 }}
+                                 ko.applyBindings(portalViewModel, null);
+                            }});</script>";
+        }
+
+        public static string MapPageData<T>(T model)
+        {
+            var json = JsonConvert.SerializeObject(model);
+            var jsOutpug = @"<script>
+                                 var viewModelJs = {0};                                            
+                                 portalViewModel.PageData.Data = ko.mapping.fromJS(viewModelJs);        
+                            </script>";
+            return string.Format(jsOutpug, json);
+
+        }
+
+    }
+}
