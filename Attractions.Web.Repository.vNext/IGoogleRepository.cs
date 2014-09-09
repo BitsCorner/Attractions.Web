5535b3344d165d9b722c2ffc5cdc819b4a99efb4
aramkoukia <aramkoukia@gmail.com>
2014-09-08 20:04:58 -0700

    Repository project vNext

--- Attractions.Web.Repository.vNext/IGoogleRepository.cs
+++ Attractions.Web.Repository.vNext/IGoogleRepository.cs
@@ -1,12 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace Attractions.Web.Repository
 {
-    public class GoogleRepository
+    public interface IGoogleRepository
     {
     }
 }
